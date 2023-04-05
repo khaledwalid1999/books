655b8ba5e6e5b5ef2d8844bfb5268ba0086bd685
Khaled <khaledabuhelal@gmail.com>
2023-04-05 11:28:46 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -22,23 +22,26 @@ function App() {
     setBooks([...books, response.data]);
   };
 
-  const handleDeleteBookById = (id) => {
-    const updatedBookList = books.filter((book) => {
-      return book.id !== id;
-    });
-    setBooks(updatedBookList);
+  const handleDeleteBook = async (id) => {
+    await axios.delete(`http://localhost:3001/books/${id}`);
+    fetchBooks();
+    // const updatedBookList = books.filter((book) => {
+    //   return book.id !== id;
+    // });
+    // setBooks(updatedBookList);
   };
 
-  const handleUpdateBook = async (id, newTitle) => {
-    const updatedBookList = books.map((book) => {
-      if (id === book.id) {
-        return { ...book, title: newTitle };
-      } else {
-        return book;
-      }
-    });
-    setBooks(updatedBookList);
-    console.log(books);
+  const handleUpdateBook = async (id, title) => {
+    await axios.put(`http://localhost:3001/books/${id}`, { title });
+    fetchBooks();
+    // const updatedBookList = books.map((book) => {
+    //   if (id === book.id) {
+    //     return { ...book, title: title };
+    //   } else {
+    //     return book;
+    //   }
+    // });
+    //setBooks(updatedBookList);
   };
 
   return (
@@ -46,7 +49,7 @@ function App() {
       <h1>Reading List</h1>
       <BookList
         onUpdate={handleUpdateBook}
-        onDelete={handleDeleteBookById}
+        onDelete={handleDeleteBook}
         books={books}
       />
       <BookCreate onSubmit={handleCreateBook} />
